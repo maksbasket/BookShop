5ef1316a2533e8bcaad2dad3d2829515c54a6594
maksbasket <maksbasket@rambler.ru>
2011-11-22 01:43:23 -0500

    Controller & Model Specs

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ActiveRecord::Base
   validates :title, :uniqueness => true
   validates :image_url, :format => {
             :with => %r{\.(gif|jpg|png)$}i,
-            :message => 'must be a URL for GIF, JPG or PNG image.'
+            :message => 'must be a URL for GIF, JPG or PNG image.'}
 
 
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,59 @@
 require 'spec_helper'
 
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {	
+     :title => 'Book',
+     :description => 'Wonderful Book',
+     :price => 10.99,
+     :image_url => 'book.jpg'
+     }
+  end
+
+  it "should create a new instance given valid attributes" do
+    Product.create!(@attr)
+  end
+  
+  it "should require a title" do
+    product = Product.new(@attr.merge(:title => ''))
+    product.should_not be_valid
+  end
+  
+  it "should require a description" do
+    product = Product.new(@attr.merge(:description => ''))
+    product.should_not be_valid
+  end
+  
+  it "should require image_url" do
+    product = Product.new(@attr.merge(:image_url => ''))
+    product.should_not be_valid
+  end
+  
+  it "should reject invalid price" do
+    product = Product.new(@attr.merge(:price => 0.009))
+    product.should_not be_valid
+  end
+  
+  it "should reject dublicate title" do
+    Product.create!(@attr)
+    product = Product.new(@attr)
+    product.should_not be_valid
+  end
+  
+  it "should accept valid image_urls" do
+    image_urls = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+                     http://a.b.c/x/y/z/fred.gif }
+    image_urls.each do |image_url|
+      product = Product.new(@attr.merge(:image_url => image_url))
+      product.should be_valid
+    end
+  end
+  
+  it "should reject invalid image_urls" do
+    image_urls = %w{ fred.doc fred.gif/more fred.gif.more }
+    image_urls.each do |image_url|
+      product = Product.new(@attr.merge(:image_url => image_url))
+      product.should_not be_valid
+    end
+  end
 end
